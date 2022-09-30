91b8fa34c2c9aca04b00fd901d58681adf37d089
Lonewolf-0 <93183924+Lonewolf-0@users.noreply.github.com>
2022-09-30 12:07:37 +0530

    recursion problems

---
+++ recursion6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void toh(int n,char x, char y, char z)
+{
+    if(n==1)
+    {
+        printf("Move disk %d from %c to %c");
+    }
+}
+
+int main()
+{
+    toh(3,'a','b','c');
+}

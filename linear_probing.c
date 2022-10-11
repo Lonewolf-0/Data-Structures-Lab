e2a918f6398cdd009ec82800b604eaa6ac6f8967
Lonewolf-0 <93183924+Lonewolf-0@users.noreply.github.com>
2022-10-11 12:35:40 +0530

    probing

--- linear_probing.c
+++ linear_probing.c
@@ -1,7 +1,4 @@
 #include <stdio.h> 
-
-
-
 int main() 
 {   
     int data[]={1000,2000,3000,4000,5000,6000};
@@ -9,9 +6,9 @@ int main()
     int dat[10]={0};
     int size=6;
     int k=0;
-    for(int i=0;i<10;i++)
-        printf("%d ",dat[i]);
-    printf("\n");
+
+    
+    
     for(int i=0;i<size;i++)
     {
         if(dat[hash_location[i]]==0)
@@ -26,7 +23,8 @@ int main()
                 {
                     dat[k]=data[i];
                     break;
-                }    
+                }  
+                  
                 
             }
             
